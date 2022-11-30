58613d7d070bdfefb67ff45262aa26e645212b5b
Lorenzo Pachioli <86329382+lorenzo-pachioli@users.noreply.github.com>
2022-11-30 17:32:09 -0300

    Sua 50 componentes dialog (#5)
    Componentes dialog created

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
-  constructor() { }
+  title: string = '';
+  content: string = '';
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<DialogComponent>
+  ) {
+    this.title = this.data.title;
+    this.content = this.data.content;
+  }
 
   ngOnInit(): void {
+  }
+
+  sendResponse(response: boolean): void {
+    this.dialogRef.close({ data: response });
   }
 
 }
