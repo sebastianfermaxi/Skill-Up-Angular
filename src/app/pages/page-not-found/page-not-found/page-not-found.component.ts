4cf311b8eff5a8414c5113665ef2891eff2f78b0
Maxidvp <72111937+Maxidvp@users.noreply.github.com>
2022-11-30 21:52:52 -0300

    SUA-24 Componente para el manejo de recurso no encontrado (404) (#9)

--- src/app/pages/page-not-found/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found/page-not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
+  @Input() msg:string = 'Recurso no encontrado'
   constructor() { }
 
   ngOnInit(): void {
