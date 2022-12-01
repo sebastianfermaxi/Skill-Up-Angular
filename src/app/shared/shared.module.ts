63862a4771c86495f743e3884d297a14f2c9dd5a
Lorenzo Pachioli <86329382+lorenzo-pachioli@users.noreply.github.com>
2022-12-01 14:34:38 -0300

    Componente Alertas (#17)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { FormComponent } from './components/form/form.component';
 import { TableComponent } from './components/table/table.component';
 import { MaterialModule } from '../material/material.module';
 import { FooterComponent } from './components/footer/footer.component';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { FooterComponent } from './components/footer/footer.component';
     FormComponent,
     TableComponent,
     FooterComponent,
+    AlertComponent,
   ],
   imports: [CommonModule, MaterialModule],
   exports: [TableComponent, FooterComponent, LoaderComponent],
