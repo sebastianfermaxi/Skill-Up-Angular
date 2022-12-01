48adc821b2708d79481d8e1bc2208c0ac9794529
Lorenzo Pachioli <86329382+lorenzo-pachioli@users.noreply.github.com>
2022-12-01 12:17:28 -0300

    Material module imports fixed (#15)

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
-import {  MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatBadgeModule } from '@angular/material/badge';
@@ -16,21 +15,21 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatStepperModule } from '@angular/material/stepper';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 
 @NgModule({
@@ -39,7 +38,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     CommonModule,
     MatButtonModule,
     MatToolbarModule,
-		MatSnackBarModule,
+    MatSnackBarModule,
     MatBadgeModule,
     MatInputModule,
     MatSelectModule,
@@ -63,16 +62,16 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatExpansionModule,
     MatTabsModule,
     MatProgressSpinnerModule,
-		MatCheckboxModule,
-		MatDatepickerModule,
-		MatNativeDateModule,
-		MatButtonToggleModule,
-		MatDividerModule
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonToggleModule,
+    MatDividerModule
   ],
   exports: [
     MatButtonModule,
     MatToolbarModule,
-		MatSnackBarModule,
+    MatSnackBarModule,
     MatBadgeModule,
     MatInputModule,
     MatSelectModule,
@@ -96,11 +95,11 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatExpansionModule,
     MatTabsModule,
     MatProgressSpinnerModule,
-		MatCheckboxModule,
-		MatDatepickerModule,
-		MatNativeDateModule,
-		MatButtonToggleModule,
-		MatDividerModule
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonToggleModule,
+    MatDividerModule
   ],
 })
 export class MaterialModule { }
