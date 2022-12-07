3c703c7e254bf16829d0182d87efbe7f39177397
Anacoronel <anacoronel@hotmail.com>
2022-12-07 17:24:54 -0300

    Sua 72 73 (#38)
    * SUA-71-componente-cotizacion-divisa
    
    * sua-72-73

--- src/app/pages/divisas/divisas-page/divisas.component.ts
+++ src/app/pages/divisas/divisas-page/divisas.component.ts
@@ -1,25 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 import { ExchangeService } from 'src/app/core/services/exchange.service';
+import { HttpService } from 'src/app/core/services/http.service';
+
+
 
 
 @Component({
-  selector: 'app-divisas',
+  selector: 'ew-divisas',
   templateUrl: './divisas.component.html',
   styleUrls: ['./divisas.component.scss']
 
 })
 export class DivisasComponent implements OnInit {
 exchange:any=[];
+res!:Number;
+form: FormGroup = new FormGroup({});
+importe = new FormControl
+resultado = new FormControl
+Monedas:any = [
+ {value: "1", viewValue: 'ARS a USD'},
+ {value: "2", viewValue: 'USD a ARS'},
+];
+seleccionada: string = this.Monedas[0].value;
+
   
 constructor(
-    private exchangeService: ExchangeService,
-  ) { }
+    private exchangeService: ExchangeService,public fb:FormBuilder,
+    public http:HttpService
+
+  ) { 
+    this.form = fb.group({
+      moneda: [''], 
+      resultado:Number,
+     importe:Number  }) 
+  }
 
   ngOnInit(): void {
     this.exchangeService.get().subscribe(( data) => {
       console.log(data)
       this.exchange= (data);
-      })
+      })    
   }
+  
+  convertir(){
+    switch(this.seleccionada) {
+      case "2" : this.res=this.exchange.blue.value_sell* this.importe.value;
+      this.resultado.setValue(this.res)
+      console.log(this.res)
+      break;
+      case "1" : this.res=this.importe.value/this.exchange.blue.value_buy
+      this.resultado.setValue(this.res)
+      console.log(this.res)
+                     break; 
+                    }
+}
+  onSubmit(){
+    this.convertir()
+    }  
+    limpiar(){
+      this.importe.reset()
+      this.resultado.reset()
+    }
+ 
 }
--- src/app/pages/divisas/divisas.module.ts
+++ src/app/pages/divisas/divisas.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material/material.module';
-
+import { FormsModule } from '@angular/forms';
 import { DivisasRoutingModule } from './divisas-routing.module';
 import { DivisasComponent } from './divisas-page/divisas.component';
 
 @NgModule({
   declarations: [DivisasComponent],
-  imports: [CommonModule, DivisasRoutingModule, MaterialModule],
+  imports: [CommonModule, DivisasRoutingModule, MaterialModule, FormsModule],
 })
 export class DivisasModule {}
